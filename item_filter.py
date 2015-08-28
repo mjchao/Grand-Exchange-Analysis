a4d2b8d8b81a7ba7bd80d223e53c070bec5226d3
Mickey Chao <mjchao2@gmail.com>
2015-08-28 13:42:39 -0400

    Get a good logistic regression classifier for volatility
    Training set accuracy is ~84.5% on 100 items. We’ll get a CV set soon.

--- item_filter.py
+++ item_filter.py
@@ -3,50 +3,13 @@
 from data_manager import DataManager
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import linear_model, datasets
-
-
-'''
-Stores data about and item. As of right now, the relevant data that we will
-use for logistic regression will be the id, price range and the maximum volume.
-The id, price range, and maximum volume should all be integer values.
-'''
-class ItemData( object ):
-    
-    '''
-    Creates an ItemData object for the object with the given ID.
-    
-    @param id - the ID of an object, as an integer
-    @param range - the price range of the object (max price - min price), as an
-    integer
-    @param maxVolume - the maximum trade volume of the object, as an integer
-    '''
-    def __init__( self , id , range , maxVolume ):
-        self._id = id
-        self._range = range
-        self._maxVolume = maxVolume
-    
-    '''
-    Returns the textual representation of this ItemData object as a 
-    comma separated list of values in the form
-    <id>,<range>,<maxVolume>
-    '''
-    def __str__( self ):
-        return  str( self._id ) + "," + str( self._range ) + "," + \
-                str( self._maxVolume )
-                
-    '''
-    Returns an array representing the ItemData object to be used in
-    training a logistic classifier.
-    '''
-    def to_array( self ):
-        return [ self._range , self._maxVolume ]
+from sklearn import linear_model
 
 '''
-Separates the promising items from the unpromising items using logistic
+Separates the volatile items from the non-volatile items using logistic
 regression.
 '''
-class Filter( object ):
+class VolatilityFilter( object ):
 
     '''
     All price data for all known commodities.
@@ -65,10 +28,9 @@ class Filter( object ):
     '''
     Creates a feature vector given the price data for a given commodity.
     Here are the features we think will be useful
-        * Standard deviation of the daily price - index 0
-        * Standard deviation of daily price changes - index 1
-        * Number of trend reversals - index 2
-        * Average volume - index 3
+        * Standard deviation of the daily price to average daily price ratio
+        * Standard deviation of daily price changes to average daily price ratio
+        * Number of trend reversals
         
     
     @param priceData - some price data for a commodity
@@ -79,14 +41,16 @@ class Filter( object ):
     def get_feature_vector( priceData ):
         datapoints = priceData.get_all_datapoints()
         dailyPrices = np.array( [ x.get_price() for x in datapoints ] )
-        dailyVolumes = np.array( [ x.get_volume() for x in datapoints ] )
+
+        #compute average daily price        
+        averageDaily = np.average( dailyPrices )
         
         #compute standard deviation of daily price
-        stddevDaily = np.std( dailyPrices )
+        stddevDaily = np.std( dailyPrices ) / averageDaily
         
         #compute standard deviation of daily price changes
         dailyChange = np.diff( dailyPrices )
-        stddevChange = np.std( dailyChange )
+        stddevChange = np.std( dailyChange ) / averageDaily
         
         #compute number of trend reversals:
         #we give all positive trend changes value 1
@@ -100,23 +64,12 @@ class Filter( object ):
         trendChanges = dailyChange
         trendChanges[ positiveTrendChanges ] = 1
         trendChanges[ negativeTrendChanges ] = 0
-        numTrendChanges = np.sum( np.diff( np.abs( trendChanges ) ) )
+        numTrendChanges = np.sum( np.abs( np.diff( trendChanges ) ) )
         
-        #compute average daily volume.
-        #if the volume was 0 on a day, we ignore that day
-        nonzeroVolumes = dailyVolumes > 0
-        dailyVolumes = dailyVolumes[ nonzeroVolumes ]
-        totalTraded = np.sum( dailyVolumes )
-        numDays = dailyVolumes.size
+        #compute standard deviation of the trend change size to price ratio
         
-        #if the item was never traded, we define it to have
-        #average volume of 0
-        if ( numDays == 0 ):
-            averageVolume = 0
-        else:
-            averageVolume = float( totalTraded ) / float( numDays )
             
-        return [ stddevDaily , stddevChange , numTrendChanges , averageVolume ]
+        return [ stddevDaily , stddevChange , numTrendChanges ]
     
     '''
     Reads in and preprocesses all the item data from the file 
@@ -127,9 +80,10 @@ class Filter( object ):
         DataManager.init()
         for id in DataManager.idToName.keys():
             priceData = DataManager.get_data_by_id( id )
-            features = Filter.get_feature_vector( priceData )
-            Filter.itemData.append( features )
-            Filter.itemDataMap[ id ] = features       
+            VolatilityFilter.priceData.append( priceData )
+            features = VolatilityFilter.get_feature_vector( priceData )
+            VolatilityFilter.itemData.append( features )
+            VolatilityFilter.itemDataMap[ id ] = features       
             
     '''
     Trains the logisitic classifier on the training data in
@@ -144,11 +98,13 @@ class Filter( object ):
             data = [int(i) for i in line.split( "," ) ]
             itemID = data[ 0 ]
             y = data[ 1 ]
-            trainingSetX.append( Filter.itemDataMap[ itemID ] )
+            trainingSetX.append( VolatilityFilter.itemDataMap[ itemID ] )
             trainingSetY.append( y )
         
-        Filter.logreg.fit( trainingSetX , trainingSetY )
-        print "Training set accuracy: " + str( Filter.logreg.score( trainingSetX , trainingSetY ) )
+        #trainingSetX = np.array( trainingSetX )
+        #scaledX = preprocessing.scale( trainingSetX )
+        VolatilityFilter.logreg.fit( trainingSetX , trainingSetY )
+        print "Training set accuracy: " + str( VolatilityFilter.logreg.score( trainingSetX , trainingSetY ) )
         
     '''
     Predicts if the given commodity with the given ID will be promising to
@@ -156,11 +112,10 @@ class Filter( object ):
     '''
     @staticmethod
     def predict( itemId ):
-        return Filter.logreg.predict( Filter.itemDataMap[ itemId ] )
+        return VolatilityFilter.logreg.predict( VolatilityFilter.itemDataMap[ itemId ] )
         
 def main():
-    Filter.init()        
-    Filter.train()
-    print Filter.predict( 11928 )
+    VolatilityFilter.init()        
+    VolatilityFilter.train()
 
 if __name__ == "__main__" : main()
